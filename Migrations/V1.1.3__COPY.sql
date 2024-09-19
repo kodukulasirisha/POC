d1a57eb5fe2dd16fce7c6699798182ee4f8de516
kodukulasirisha <141317290+kodukulasirisha@users.noreply.github.com>
2024-09-19 12:18:30 +0000

    test

--- Migrations/V1.1.3__COPY.sql
+++
@@ -1,7 +0,0 @@
-use schema REP_VIEW;
-CREATE TASK test_task
- WAREHOUSE = {{warehouse_name}} //warehouse name value will change according to the       workflow file which gets triggered according to the branch
- SCHEDULE = '60 minute'
-AS
-SELECT CURRENT_TIMESTAMP;
-alter task test_task suspend;
