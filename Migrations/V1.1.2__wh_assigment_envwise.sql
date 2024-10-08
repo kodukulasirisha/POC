85aaafab3a77c20887ea3305aa5f1ccb3cfda2ef
kodukulasirisha <141317290+kodukulasirisha@users.noreply.github.com>
2024-10-08 16:17:54 +0530

    Update V1.1.2__wh_assigment_envwise.sql

--- Migrations/V1.1.2__wh_assigment_envwise.sql
+++ Migrations/V1.1.2__wh_assigment_envwise.sql
@@ -1,4 +1,4 @@
-use schema CI_CD_SCHEMA;
+use schema CI_CD_POC;
 CREATE TASK test_task
  WAREHOUSE = {{warehouse_name}} //warehouse name value will change according to the       workflow file which gets triggered according to the branch
  SCHEDULE = '60 minute'
